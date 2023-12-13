604fc927a103e06b1833ffa093e14b5610be54f9
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2023-12-13 13:58:03 -0500

    finish

--- Assets/scripts/PlayerControlle.cs
+++ Assets/scripts/PlayerControlle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControlle : MonoBehaviour
 {
@@ -48,6 +49,7 @@ public class PlayerControlle : MonoBehaviour
         if(collision.gameObject.tag == "Enemigo")
         {
             Destroy(this.gameObject);
+
         }
     }
     private void FixedUpdate()
